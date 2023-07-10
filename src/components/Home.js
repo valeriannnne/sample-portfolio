fdb1a1ac6c6ed95ed3de414383eb482614953569
valeriannnne <valerie.liann@gmail.com>
2023-07-10 11:59:01 +0800

    home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,82 @@
 import React from 'react';
 import Image from '../assets/avatar.png';
-import {FaGithub, FaYoutube, FaDribble} from 'react-icons/fa';
-import {TypeAnimation, motion} from 'react-type-animation';
+import {FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa';
+import {TypeAnimation} from 'react-type-animation';
+import {motion} from 'framer-motion';
 import {fadeIn} from '../variants';
 
 const Home = () => {
-  return <div className='section' id='home'>
+  return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
     <div className='container mx-auto'>
-      {/* text */}
-      <div>
-        <h1>
-          Lianne <span>Serafica</span>
-        </h1>
-        <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
-          <span className='mr-4 text-white'>Hi! I am an aspiring </span>
-          <TypeAnimation sequence={
-            ['Web Developer', 2000,
-            'UI/UX Designer', 2000,]}
-            speed={50}
-            className='text-accent'
-            wrapper='span'
-            repeat={Infinity}
-            />
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center'>
+        {/* text */}
+        <div className='flex-1 text-center font-secondary lg:text-left'>
+          <motion.h1 
+          variants={fadeIn('up',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once:false, amount:0.7}}
+          className='text-[42px] font-bold leading-[0.8] lg:text-[72px]'>
+            Lianne <span>Serafica</span>
+          </motion.h1>
+          <motion.div 
+          variants={fadeIn('up',0.4)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once:false, amount:0.7}}
+          className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+            <span className='mr-4 text-white'>Hi! I'm an aspiring </span>
+            <TypeAnimation sequence={
+              ['Web Developer', 2000,
+              'UI/UX Designer', 2000,]}
+              speed={50}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+              />
+          </motion.div>
+          <motion.p 
+          variants={fadeIn('up',0.5)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once:false, amount:0.7}}
+          className='mb-8 max-w-lg mx-auto lg:mx-0'>
+            I am a Third Year student from Polytechnic University of the Philippines. I am currently taking up Bachelor of Science in Computer Science.
+          </motion.p>
+          <motion.div 
+          variants={fadeIn('up',0.6)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once:false, amount:0.7}}
+          className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            <button className='btn btn-lg'>Contact Me</button>
+            <a href="#" className='text-gradient btn-link'>My Portfolio</a>
+          </motion.div>
+          {/* Socials */}
+          <motion.div 
+          variants={fadeIn('up',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once:false, amount:0.7}}
+          className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            <a href='#'><FaFacebook/></a>
+            <a href='#'><FaGithub/></a>
+            <a href='#'><FaLinkedin/></a>
+          </motion.div>
         </div>
-        <p>
-          I am a Third Year student from Polytechnic University of the Philippines. I am currently taking up Bachelor of Science in Computer Science.
-        </p>
-        <div>
-          <button className='btn btn-lg'>Contact Me</button>
-          <a href="#" className='text-gradient btn-link'>My Portfolio</a>
-        </div>
-      </div>
-      {/* image */}
-      <div>
-        <img src={Image}/>
+      
+        {/* My Picture */}
+        <motion.div 
+        variants={fadeIn('down',0.5)}
+        initial='hidden'
+        whileInView={'show'}
+        className='hidden lg:flex flex-1 max-w-[320px] lg:max-[482px] w-96'>
+          <img src={Image}/>
+        </motion.div>
       </div>
 
     </div>
-  </div>;
+  </section>;
 };
 
 export default Home;
