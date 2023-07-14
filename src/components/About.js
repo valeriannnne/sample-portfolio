f13edc407cc55e1ec52deeb21e0b04f63f72586e
valeriannnne <valerie.liann@gmail.com>
2023-07-14 12:27:48 +0800

    services and updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -33,38 +33,41 @@ const About = () => {
           <div className='flex-1'>
             <h2 className='h2 text-accent'>About Me</h2>
             <h3 className='h3 mb-4'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              Front-End Developer 
             </h3>
             <p className='mb-6'>
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            I am a third year student from Polytechnic University of the Philippines Manila. I am currently enrolled in Bachelor of Science in Computer Science. I have basic knowledge in Web Development. As a diligent and hardworking student, I believe that I can achieve the tasks that will be assigned to me.
+            <br></br><br></br>
+            I am aware of the basics needed for Web Development such as using HTML, CSS, PHP, JavaScript, SQL, and other frameworks like ReactJS, NextJS, Bootstrap CSS, and Tailwind CSS. I also have background in using Android Studio, Figma, and  UIzard. 
+
             </p>
           </div>       
           <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
             <div>
               <div className='text-[40px] font-tertiary text-gradient mb-2'>
                 {inView ? <CountUp start={0} end={13} duration={3}/> : null} {/*  not showing*/}
-                <CountUp start={0} end={13} duration={3}/>
+                <CountUp start={0} end={22} duration={3}/>
               </div>
               <div className='font-primary text-sm tracking-[2px]'>
-                Years of <br/> Experience
+                Years <br/> Old
+              </div>
+            </div>
+            <div>
+              <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                {inView ? <CountUp start={0} end={10} duration={3}/> : null} {/*  not showing*/}
+                <CountUp start={0} end={15} duration={3}/>+
+              </div>
+              <div className='font-primary text-sm tracking-[2px]'>
+                Past <br/> Projects
               </div>
             </div>
             <div>
               <div className='text-[40px] font-tertiary text-gradient mb-2'>
                 {inView ? <CountUp start={0} end={13} duration={3}/> : null} {/*  not showing*/}
-                <CountUp start={0} end={15} duration={3}/>k+
+                <CountUp start={0} end={1} duration={3}/>
               </div>
               <div className='font-primary text-sm tracking-[2px]'>
-                Years of <br/> Experience
-              </div>
-            </div>
-            <div>
-              <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                {inView ? <CountUp start={0} end={13} duration={3}/> : null} {/*  not showing*/}
-                <CountUp start={0} end={13} duration={3}/>
-              </div>
-              <div className='font-primary text-sm tracking-[2px]'>
-                Years of <br/> Experience
+                Year of <br/> Experience
               </div>
             </div>
           </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import {BsArrowUpRight} from 'react-icons/bs';
+import {motion} from 'framer-motion'
+import {fadeIn} from '../variants';
+
+//services
+const services = [
+  {
+    name:"UI/UX Designer",
+    description:"tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+    link: 'Learn More'
+  },
+  {
+    name:"Front-End Developer",
+    description:"tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+    link: 'Learn More'
+  },
+  {
+    name:"Software Developer",
+    description:"tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+    link: 'Learn More'
+  },
+]
 
 const Services = () => {
-  return <div className='section' id='services'>Services</div>;
+  return <section className='section' id='services'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row'>
+        {/* text and image */}
+        <motion.div 
+        variants={fadeIn('left', 0.3)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false, amount: 0.3}}
+        className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0'>
+          <h2 className='h2 text-accent mb-6'>What I Do</h2>
+          <h3 className='h3 mb-16 max-w-[455px]'>
+            tempor incididunt ut labore et dolore magna aliqua.
+          </h3>
+          <button className='btn btn-sm'>See my Work</button>
+        </motion.div>
+        {/* services */}
+        <motion.div
+        variants={fadeIn('right', 0.5)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false, amount: 0.3}}
+        >
+          {/* services list */}
+          <div className='flex-1'>
+            {
+              services.map((service, index) => {
+                // destructure data
+                const {name, description, link} = service
+                return(
+                  <div className='flex flex-row border-b border-white/20 h-[146px] mb-[38px]' key={index}>
+                    <div className='max-w-[476px]'>
+                      <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>{name}</h4>
+                      <p className='font-secondary leading-tight'>{description}</p>
+                    </div>
+                    <div className='flex flex-col flex-1 items-end'> 
+                      <a href='#' className='btn w-9 h-9 mb-[42px] flex justify-center items-center'><BsArrowUpRight/></a>
+                      <a href='#' className='text-gradient text-sm'>{link}</a>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Services;
